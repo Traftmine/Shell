fe5deb99b12515deaa8285cddc141eb6b4daa479
Traftmine <leo.royer.1@etu.u-bordeaux.fr>
2023-10-15 14:52:02 +0200

    first add

---
+++ Evaluation.c
@@ -0,0 +1,12 @@
+#include "Evaluation.h"
+#include "Shell.h"
+
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int evaluateExpr(Expression *expr) {
+  return shellStatus;
+}
