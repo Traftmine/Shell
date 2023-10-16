920fb6a540df9996d037c07dfc94a9076ac9baae
Traftmine <leo.royer.1@etu.u-bordeaux.fr>
2023-10-16 09:17:52 +0200

    redirect output input fonctionne

--- Evaluation.c
+++ Evaluation.c
@@ -10,104 +10,23 @@
 // ------ FUNCTIONS ------ //
 
 int valeurStatus(int status);
+int simpleCommand(Expression *expr);
+int RedirectCommand(Expression *expr);
 
 // ---- MAIN FUNCTION ---- //
 
 int evaluateExpr(Expression *expr) {
 
-  if (expr->type == ET_EMPTY) { // Commande vide
-    fprintf(stderr, "Expression is empty\n");
-    return EXIT_SUCCESS;
-  }
-
-  int status;
-
-  // Extraction de la commande et de ses arguments de l'expression //
-  char *command = expr->argsList.args[0]; // Commande principale
-
-  char *args[64];
-  for (int i = 0; i < expr->argc; i++){
-    args[i] = expr->argv[i];
-  }
-  args[expr->argc] = NULL;
-
-  pid_t pid;
-
   switch (expr->type) {
+    case ET_EMPTY:
+      fprintf(stderr, "Expression is empty\n");
+      break;
     case ET_SIMPLE: // Commande simple
-      pid = fork();
-      if (pid == -1) {
-        // Erreur lors de la création du processus fils
-        perror("fork");
-        return -1;
-      }
-      if (pid == 0) {
-        // Code du processus fils
-        execvp(command, args);
-        // Si execvp renvoie, il y a eu une erreur
-        perror("execvp");
-        exit(1); // Termine le processus fils en cas d'échec
-      } else {
-        // Code du processus parent
-        if (waitpid(pid, &status, 0) == -1) {
-          perror("waitpid");
-          return -1;
-        }
-        return valeurStatus(status);
-      }
-
+      simpleCommand(expr);
       break;
     case ET_REDIRECT: // Redirection
-      switch (expr->redirect.type) {
-        case REDIR_OUT: // Sortie (> ou >&)
-          pid = fork();
-          if (pid == 0) {
-            int out = open(expr->redirect.fileName, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-            if (out == -1) {
-              perror("open");
-              exit(1);
-            }
-            dup2(out, expr->redirect.fd); // Redirige stdout vers le fichier
-            close(out);
-            execvp(command, args);
-            perror("execvp");
-            exit(127);
-          }
-          waitpid(pid, NULL, 0);
-          break;
-        case REDIR_IN: // Entrée (< ou <&)
-          pid = fork();
-          if (pid == 0) {
-            int in = open(expr->redirect.fileName, O_RDONLY);
-            if (in == -1) {
-              perror("open");
-              exit(1);
-            }
-            dup2(in, expr->redirect.fd); // Redirige stdin depuis le fichier
-            close(in);
-            execvp(command, args);
-            perror("execvp");
-            exit(127);
-          }
-          waitpid(pid, NULL, 0);
-          break;
-        case REDIR_APP: // Sortie, mode ajout (>>)
-          pid = fork();
-          if (pid == 0) {
-            int out = open(expr->redirect.fileName, O_CREAT | O_WRONLY | O_APPEND, 0644);
-            if (out == -1) {
-                perror("open");
-                exit(1);
-            }
-            dup2(out, STDOUT_FILENO); // Redirige stdout vers le fichier en mode prolongation (>>)
-            close(out);
-            execvp(command, args);
-            perror("execvp");
-            exit(127);
-          }
-          waitpid(pid, NULL, 0);
-          break;
-      }
+      RedirectCommand(expr);
+      break;
     case ET_SEQUENCE: // Séquence (;)
       break;
     case ET_SEQUENCE_AND: // Séquence conditionnelle (&&)
@@ -132,4 +51,100 @@ int valeurStatus(int status) {
     return WEXITSTATUS(status);
   }
   return 128 + WTERMSIG(status);
+}
+
+// --- Simple Command ---//
+
+int simpleCommand(Expression *expr){
+  int status;
+  // Extraction de la commande et de ses arguments de l'expression //
+  char *command = expr->argsList.args[0]; // Commande principale
+
+  char *args[64];
+  for (int i = 0; i < expr->argc; i++){
+    args[i] = expr->argv[i];
+  }
+  args[expr->argc] = NULL;
+
+  pid_t pid = fork();
+  if (pid == -1) {
+    // Erreur lors de la création du processus fils
+    perror("fork");
+    return -1;
+  }
+  if (pid == 0) {
+    // Code du processus fils
+    execvp(command, args);
+    // Si execvp renvoie, il y a eu une erreur
+    perror("execvp");
+    exit(1); // Termine le processus fils en cas d'échec
+  } else {
+    // Code du processus parent
+    if (waitpid(pid, &status, 0) == -1) {
+      perror("waitpid");
+      return -1;
+    }
+    return valeurStatus(status);
+  }
+}
+
+// --- Redirect Command ---//
+
+int RedirectCommand(Expression *expr){
+  pid_t pid;
+
+  switch (expr->redirect.type) {
+    case REDIR_OUT: // Sortie (> ou >&)
+      pid = fork();
+      if (pid == 0) {
+        int out = open(expr->redirect.fileName, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+        if (out == -1) {
+          perror("open");
+          exit(1);
+        }
+        dup2(out, expr->redirect.fd); // Redirige stdout vers le fichier
+        close(out);
+        execvp(expr->left->argv[0],expr->left->argv);
+        perror("execvp");
+        exit(127);
+      }
+      waitpid(pid, NULL, 0);
+      break;
+    case REDIR_IN: // Entrée (< ou <&)
+      pid = fork();
+      if (pid == 0) {
+        int in = open(expr->redirect.fileName, O_RDONLY);
+        if (in == -1) {
+          perror("open");
+          exit(1);
+        }
+        dup2(in, expr->redirect.fd); // Redirige stdin depuis le fichier
+        close(in);
+        execvp(expr->left->argv[0],expr->left->argv);
+        perror("execvp");
+        exit(127);
+      }
+      waitpid(pid, NULL, 0);
+      break;
+    case REDIR_APP: // Sortie, mode ajout (>>)
+      pid = fork();
+      if (pid == 0) {
+        int out = open(expr->redirect.fileName, O_CREAT | O_WRONLY | O_APPEND, 0644);
+        if (out == -1) {
+            perror("open");
+            exit(1);
+        }
+        dup2(out, STDOUT_FILENO); // Redirige stdout vers le fichier en mode prolongation (>>)
+        close(out);
+        simpleCommand(expr);
+        perror("execvp");
+        exit(127);
+      }
+      waitpid(pid, NULL, 0);
+      break;
+    default:
+      fprintf(stderr,"No redirect command valid");
+      break;
+  }
+  return shellStatus;
 }
